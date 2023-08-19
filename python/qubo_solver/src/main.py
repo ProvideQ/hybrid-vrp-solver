9c34eb9f5d3a0104716c3bb85e130b1fcbc3eb82
Lucas Berger <lucask.berger@gmail.com>
2023-08-19 15:32:56 +0200

    pure dwave sampler with embedding composite

--- python/qubo_solver/src/main.py
+++ python/qubo_solver/src/main.py
@@ -1,26 +1,24 @@
 import datetime
 from dimod import BINARY, BinaryQuadraticModel
-from dwave.cloud import Client
+from dwave.system import DWaveSampler, EmbeddingComposite
 from datetime import datetime
 
 
 def main():
     # Your main code logic goes here
-    with Client.from_config() as client:
-        solver = client.get_solver()
         
-        with open('./att48_1.coo') as problem:
-            bqm = BinaryQuadraticModel.from_coo(problem, vartype=BINARY)
-            len(bqm)
-            last = datetime.now().timestamp()
-            print("started")
-            future = solver.sample_bqm(bqm, time_limit=3)
-            future.wait()
-            now = datetime.now().timestamp()
-            print(f"ended after {now - last}")
-            sampleset = future.result()['sampleset']
-            print(sampleset.first.energy)
-            print(sampleset.first.sample)
+    with open('./att48_0.coo') as problem:
+        bqm = BinaryQuadraticModel.from_coo(problem, vartype=BINARY)
+        len(bqm)
+        last = datetime.now().timestamp()
+        print("started")
+        sampleset = EmbeddingComposite(DWaveSampler()).sample(bqm, num_reads=1000, label='DWaveSampler with embedding num_reads=1000')
+        # future.wait()
+        now = datetime.now().timestamp()
+        print(f"ended after {now - last}")
+        # sampleset = future.result()['sampleset']
+        print(sampleset.first.energy)
+        print(sampleset.first.sample)
 
 if __name__ == "__main__":
     main()
